7f4874095c39d884119d7e133c16d6c41ba6249a
An1honyY <ant.jo.yuen@gmail.com>
2023-10-12 02:08:35 +1300

    clean up unnecessary code for select

--- frontend/src/components/search.js
+++ frontend/src/components/search.js
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './search.module.css';
 import Select from 'react-select';
 
@@ -24,46 +24,24 @@ const claims = [
 const claimsMap = claims.map(opt => ({ label: opt, value: opt }));
 
 export default function Search({ onSearch }) {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [filteredMethods, setFilteredMethods] = useState(softwareEngineeringMethods);
-    const [isDropdownVisible, setIsDropdownVisible] = useState(false);
+    const [selectedMethod, setSelectedMethod] = useState('');
     const [selectedClaim, setSelectedClaim] = useState('');
+    const [searchTerm, setSearchTerm] = useState('');
 
-    useEffect(() => {
-        // Update filtered methods whenever the softwareEngineeringMethods change
-        setFilteredMethods(softwareEngineeringMethods);
-    }, []);
-
-    const handleInputChange = (e) => {
-        const query = e.target.value.toLowerCase();
-        setSearchTerm(query);
-
-        const filteredMethods = softwareEngineeringMethods.filter(method =>
-            method.toLowerCase().includes(query)
-        );
-
-        setFilteredMethods(filteredMethods);
+    const handleMethodChange = (opt) => {
+        setSelectedMethod(opt.value);
     };
 
-    /*
-    * Dropdown:
-    * When a method is clicked, update the search term and hide the dropdown
-    */
-    const handleDropdownItemClick = (method) => {
-        console.log('Method clicked:', method);
-        setSearchTerm(method);
-        setIsDropdownVisible(false);
+    const handleClaimChange = (opt) => {
+        setSelectedClaim(opt.value);
     };
+
 
     const handleSearch = () => {
-        // Pass the search term to the parent component
-        onSearch(searchTerm);
+        const fullSearchTerm = `${selectedMethod} ${selectedClaim}`;
+        setSearchTerm(fullSearchTerm);
+        onSearch(fullSearchTerm);
     };
-
-    const handleClaimChange = (e) => {
-        setSelectedClaim(e.target.value);
-    };
-
 
     return (
         <>
@@ -73,27 +51,9 @@ export default function Search({ onSearch }) {
                     <a className={styles.h2}>Search a Claim:</a>
 
                     <a className={styles.h3}>Select a Method:</a>
-                    <Select className={styles.select} options={methodsMap} onChange={opt => console.log(opt.label, opt.value)} />
+                    <Select className={styles.select} options={methodsMap} onChange={handleMethodChange} />
                     <a className={styles.h3}>Select a Claim:</a>
-                    <Select className={styles.select} options={claimsMap} onChange={opt => console.log(opt.label, opt.value)} />
-
-
-
-
-                    {/* Dropdown*/}
-                    {isDropdownVisible && (
-                        <div className={styles.dropdown} onClick={(e) => e.stopPropagation()}>
-                            {filteredMethods.map((method, index) => (
-                                <div
-                                    key={index}
-                                    className={styles.dropdownItem}
-                                    onClick={() => handleDropdownItemClick(method)}
-                                >
-                                    {method}
-                                </div>
-                            ))}
-                        </div>
-                    )}
+                    <Select className={styles.select} options={claimsMap} onChange={handleClaimChange} />
 
                     {/*Button Search*/}
                     <button className={styles.searchButton} onClick={handleSearch}>
@@ -102,6 +62,14 @@ export default function Search({ onSearch }) {
                 </div>
 
             </div>
+            {/* Display the search term */}
+            {searchTerm && (
+                <div className={styles.outerContainer}>
+                    <div className={styles.searchContainer}>
+                        <p>Search Term: {searchTerm}</p>
+                    </div>
+                </div>
+            )}
         </>
     );
 }
