2722d82908179b2eba2944312fe6a49f32a6bb18
LeoLe00 <141303214+LeoLe00@users.noreply.github.com>
2023-10-12 12:37:13 +1300

    Add files via upload

---
+++ backend/routes/api/suggest.js
@@ -0,0 +1,34 @@
+// routes/suggest.js
+const express = require('express');
+const mongoose = require('mongoose');
+
+const router = express.Router();
+
+const connectionString = 'YOUR_MONGODB_ATLAS_CONNECTION_STRING'; // from MongoDB Atlas
+
+mongoose.connect(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Article = mongoose.model('Article', {
+  author: String,
+  title: String,
+  journal: String,
+  year: String,
+  volume: String,
+  number: String,
+  pages: String,
+});
+
+router.post('/suggest', async (req, res) => {
+  try {
+    const newArticle = new Article(req.body);
+    await newArticle.save();
+    res.status(200).send({ message: 'Article submitted successfully.' });
+  } catch (error) {
+    res.status(500).send({ error: 'Failed to submit the article.' });
+  }
+});
+
+module.exports = router;
