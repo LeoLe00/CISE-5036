5b21bf417b1b3b48aa567eebbabf1d3dfa20bea2
LeoLe00 <141303214+LeoLe00@users.noreply.github.com>
2023-10-12 16:29:11 +1300

    Merge branch 'main' into anthony

--- frontend/src/components/articleForm.js
+++ frontend/src/components/articleForm.js
@@ -23,6 +23,7 @@ export default function ArticleForm() {
       number,
       pages,
     });
+    console.log(articleData);
 
     try {
       const response = await fetch('https://cise-5036.vercel.app/api/suggest', { 
@@ -30,7 +31,7 @@ export default function ArticleForm() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(log),
+        body: JSON.stringify(articleData),
       });
   
       const result = await response.json();
