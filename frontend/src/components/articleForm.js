638d2003dbce33a1cc339e5a7b29ba15fc3a4511
LeoLe00 <141303214+LeoLe00@users.noreply.github.com>
2023-10-19 09:51:51 +1300

    Add files via upload

---
+++ frontend/src/components/articleForm.js
@@ -0,0 +1,124 @@
+"use client";
+import { useState } from 'react';
+import styles from './articleForm.module.css';
+
+export default function ArticleForm() {
+  const [author, setAuthor] = useState('');
+  const [title, setTitle] = useState('');
+  const [journal, setJournal] = useState('');
+  const [year, setYear] = useState('');
+  const [volume, setVolume] = useState('');
+  const [number, setNumber] = useState('');
+  const [pages, setPages] = useState('');
+  const [message, setMessage] = useState(null);
+
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+   
+    const articleData = {
+      author,
+      title,
+      journal,
+      year,
+      volume,
+      number,
+      pages,
+    };
+
+    try {
+      const response = await fetch('http://localhost:8082/api/suggest', { 
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(articleData),
+      });
+  
+      const result = await response.json();
+  
+      if (response.status === 200) {
+        setMessage(result.message); 
+      } else {
+        setMessage(result.error || 'Something went wrong.');
+      }
+    } catch (error) {
+      console.error('Error while submitting the article:', error);
+      setMessage('An unexpected error occurred.');
+    }
+  };
+
+  return (
+    <div className={styles.outerContainer}>
+      <div className={styles.articleFormContainer}>
+        <h2>Research Article Suggestion Form</h2>
+        {message && <div className={styles.message}>{message}</div>}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="author">Author(s):</label>
+            <input
+              type="text"
+              id="author"
+              value={author}
+              onChange={(e) => setAuthor(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="title">Title:</label>
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="journal">Journal:</label>
+            <input
+              type="text"
+              id="journal"
+              value={journal}
+              onChange={(e) => setJournal(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="year">Year:</label>
+            <input
+              type="text"
+              id="year"
+              value={year}
+              onChange={(e) => setYear(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="volume">Volume:</label>
+            <input
+              type="text"
+              id="volume"
+              value={volume}
+              onChange={(e) => setVolume(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="number">Number:</label>
+            <input
+              type="text"
+              id="number"
+              value={number}
+              onChange={(e) => setNumber(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="pages">Pages:</label>
+            <input
+              type="text"
+              id="pages"
+              value={pages}
+              onChange={(e) => setPages(e.target.value)}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
